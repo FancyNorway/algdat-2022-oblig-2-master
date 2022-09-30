caf32507ee30a923445ee4dad9fe6f4b36e89ed2
Maggielicious <magnus.belsvik@gmail.com>
2022-09-30 16:51:25 +0200

    jobbet litt videre på oppgave 1 :)

--- src/main/java/no/oslomet/cs/algdat/Oblig2/DobbeltLenketListe.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig2/DobbeltLenketListe.java
@@ -53,9 +53,11 @@ public class DobbeltLenketListe<T> implements Liste<T> {
 
 
         for(int i = 0; i < a.length; i++){
+            if(a[i] != null){
+                current.verdi = a[i];
+                current = current.neste;
+            }
 
-            current.verdi = a[i];
-            current = current.neste;
 
         }
 
