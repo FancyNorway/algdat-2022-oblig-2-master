3004abb16008e0614f3ec2b4e36b2aacc71ba264
Anders E.H <s362061@oslomet.no>
2022-09-30 17:31:54 +0200

    legger til en testclasse

---
+++ src/main/java/no/oslomet/cs/algdat/Oblig2/MainTest.java
@@ -0,0 +1,9 @@
+package no.oslomet.cs.algdat.Oblig2;
+
+public class MainTest {
+    public static void main(String[] args) {
+        String[] s = {"Ole", null, "Per", "Kari", null};
+        Liste<String> liste = new DobbeltLenketListe<>(s);
+        System.out.println(liste.antall() + " " + liste.tom());
+    }
+}
