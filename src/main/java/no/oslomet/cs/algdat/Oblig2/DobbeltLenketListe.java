a17f06068932a92d4b638f5582740f23cd3fa71f
Maggielicious <magnus.belsvik@gmail.com>
2022-10-01 17:57:04 +0200

    gjorde ferdig oppgave 5, men må fikse litt igjen på oppgave 1 og 2

--- src/main/java/no/oslomet/cs/algdat/Oblig2/DobbeltLenketListe.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig2/DobbeltLenketListe.java
@@ -101,7 +101,7 @@ public class DobbeltLenketListe<T> implements Liste<T> {
             return antall = 0;
         }
         int antall = 0;
-        Node current = hode;
+        Node current = hode.neste;
 
         while(current.neste != null){
             antall++;
